eaccd27836d5a1e34749f738cc637a5a290f5394
DiegoFS <100387203@alumnos.uc3m.es>
2022-06-21 13:16:53 +0200

    nuevos cambios

---
+++ script/search/search.js
@@ -0,0 +1,10 @@
+class Node{
+    constructor(explorer_x, explorer_y, objective_x, objective_y){
+        this.parent = None;
+        this.explorer_x = explorer_x;
+        this.explorer_y = explorer_y;
+        this.objective_x = objective_x;
+        this.objective_y = objective_y;
+    }
+
+}
